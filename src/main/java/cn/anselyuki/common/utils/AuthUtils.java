ddf5c47a7cf41d8ad6873e9b767005a33175af85
AnselYuki <gongyihang0724@gmail.com>
2023-05-03 17:16:52 +0800

    refactor:提取工具类,调整用户认证逻辑

---
+++ src/main/java/cn/anselyuki/common/utils/AuthUtils.java
@@ -0,0 +1,40 @@
+package cn.anselyuki.common.utils;
+
+import cn.hutool.core.convert.NumberWithFormat;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.jwt.JWT;
+import cn.hutool.jwt.JWTUtil;
+import cn.hutool.jwt.RegisteredPayload;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuthUtils {
+    public static String extractToken(HttpServletRequest request, String header) throws Exception {
+        if (SecurityContextHolder.getContext().getAuthentication() != null) throw new Exception("no need to auth");
+        var head = request.getHeader(header);
+        if (head == null || !head.startsWith("Bearer ")) throw new Exception("token not found");
+        return head.substring(7);
+    }
+
+    public static JWT parseAndValidateJwt(String token, String secret) throws BadCredentialsException {
+        if (!JWTUtil.verify(token, secret.getBytes()))
+            throw new BadCredentialsException("invalid token");
+        var jwt = JWTUtil.parseToken(token);
+        var now = DateTime.now();
+
+        NumberWithFormat format = (NumberWithFormat) jwt.getPayload(RegisteredPayload.NOT_BEFORE);
+        long longValue = format.longValue();
+
+        var notBefore = DateTime.of(longValue);
+        if (now.isBefore(notBefore)) throw new CredentialsExpiredException("haven't come into effect");
+
+        format = (NumberWithFormat) jwt.getPayload(RegisteredPayload.EXPIRES_AT);
+        longValue = format.longValue();
+
+        var expiresAt = DateTime.of(longValue);
+        if (now.isAfter(expiresAt)) throw new CredentialsExpiredException("token expired");
+        return jwt;
+    }
+}
--- src/main/java/cn/anselyuki/security/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/cn/anselyuki/security/filter/JwtAuthenticationTokenFilter.java
@@ -1,12 +1,9 @@
 package cn.anselyuki.security.filter;
 
+import cn.anselyuki.common.utils.AuthUtils;
 import cn.anselyuki.common.utils.RedisCache;
 import cn.anselyuki.security.LoginUser;
-import cn.hutool.core.convert.NumberWithFormat;
-import cn.hutool.core.date.DateTime;
 import cn.hutool.jwt.JWT;
-import cn.hutool.jwt.JWTUtil;
-import cn.hutool.jwt.RegisteredPayload;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -31,63 +27,37 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @SuppressWarnings("NullableProblems")
 public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
-    private final RedisCache redisCache;
     @Value("${jwt.secret}")
-    private String secret;
+    private static String secret;
     @Value("${jwt.header}")
-    private String header;
+    private static String header;
     @Value("${jwt.enabled}")
-    private Boolean enabled;
+    private static Boolean enabled;
+    private final RedisCache redisCache;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
         if (!enabled) {
-            log.warn("JWT认证已禁用,请仅在测试环境中禁用");
+            log.warn("JWT authentication has been disabled, please disable it only in the testing environment");
             filterChain.doFilter(request, response);
             return;
         }
         try {
-            var token = extractToken(request);
-            var jwt = parseAndValidateJwt(token);
+            var token = AuthUtils.extractToken(request, header);
+            var jwt = AuthUtils.parseAndValidateJwt(token, secret);
             var user = retrieveAndValidateUser(jwt);
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (Exception e) {
             log.info(e.getMessage());
         } finally {
-            if (Objects.nonNull(SecurityContextHolder.getContext().getAuthentication()))
+            if (Objects.nonNull(SecurityContextHolder.getContext().getAuthentication())) {
                 log.info("[{}] Access [{}]",
                         SecurityContextHolder.getContext().getAuthentication().getName(),
                         request.getRequestURI());
+            }
             filterChain.doFilter(request, response);
         }
-    }
-
-    private String extractToken(HttpServletRequest request) throws Exception {
-        if (SecurityContextHolder.getContext().getAuthentication() != null) throw new Exception("no need to auth");
-        var head = request.getHeader(header);
-        if (head == null || !head.startsWith("Bearer ")) throw new Exception("token not found");
-        return head.substring(7);
-    }
-
-    private JWT parseAndValidateJwt(String token) throws BadCredentialsException {
-        if (!JWTUtil.verify(token, secret.getBytes()))
-            throw new BadCredentialsException("invalid token");
-        var jwt = JWTUtil.parseToken(token);
-        var now = DateTime.now();
-
-        NumberWithFormat format = (NumberWithFormat) jwt.getPayload(RegisteredPayload.NOT_BEFORE);
-        long longValue = format.longValue();
-
-        var notBefore = DateTime.of(longValue);
-        if (now.isBefore(notBefore)) throw new CredentialsExpiredException("haven't come into effect");
-
-        format = (NumberWithFormat) jwt.getPayload(RegisteredPayload.EXPIRES_AT);
-        longValue = format.longValue();
-
-        var expiresAt = DateTime.of(longValue);
-        if (now.isAfter(expiresAt)) throw new CredentialsExpiredException("token expired");
-        return jwt;
     }
 
     private LoginUser retrieveAndValidateUser(JWT jwt) throws AuthenticationException {
