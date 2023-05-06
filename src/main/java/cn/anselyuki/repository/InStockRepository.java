979d20fa85c3d60d9da1e55b7a17851267e03f86
AnselYuki <gongyihang0724@gmail.com>
2023-05-06 23:33:31 +0800

    refactor: 删除入库模块

--- src/main/java/cn/anselyuki/repository/InStockRepository.java
+++
@@ -1,7 +0,0 @@
-package cn.anselyuki.repository;
-
-import cn.anselyuki.repository.entity.InStock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InStockRepository extends JpaRepository<InStock, String> {
-}
